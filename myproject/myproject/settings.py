af2231b3bbec9424eb39564b674ba85cc2c981db
PNS-Srivardhan <211801410005@cutmap.ac.in>
2024-10-31 16:34:13 +0530

    changed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -58,7 +58,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
-        'NAME': r'C:\Users\ploke\SMS\mos-main\myproject\db.sqlite3',
 
         
 
